f2040e4cb3c1bdd5a255b4931343723ebceda915
Md Kashif <kashif124adx@gmail.com>
2026-05-12 02:31:38 +0530

    patternProgram

---
+++ C8_Pattern2/code2_StarPyramid.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter num of Stars : ";
+    cin>>n;
+
+    for(int i = 1; i <= n; i++){
+        int k;
+        for(k = 1; k <= n-i; k++){
+            cout<<"  ";
+        }
+        for(int j = 1; j <= n-k+i; j++){
+            cout<<"* ";
+        }
+    cout<<endl;
+    }
+}
